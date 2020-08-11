00d9d21a3a2e2729781e63a610ccdfb47f308ca7
Joe Sweeney <joe.sweeney224@gmail.com>
2020-08-10 19:05:40 -0600

    initial commit

---
+++ src/App.svelte
@@ -0,0 +1,14 @@
+{#if name}
+<h1 >Hey there {name}!</h1>
+{/if}
+<input type="text" bind:value={name}>
+<button on:click={countUp}>I clicked this {count} times.</button>
+
+<script>
+  let name = ''
+  let count = 0
+
+  function countUp() {
+    count += 1
+  }
+</script>
