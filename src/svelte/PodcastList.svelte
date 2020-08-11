a252e8f733de902286afedaa3e2f9e05f2b7f24e
Joe Sweeney <joe.sweeney224@gmail.com>
2020-08-10 21:30:28 -0600

    list podcast images

--- src/svelte/PodcastList.svelte
+++ src/svelte/PodcastList.svelte
@@ -1,1 +1,24 @@
-<h2>Some podcasts</h2>
+<div class="flex flex-wrap">
+  {#each podcasts as pc}
+  <div class="w-1/3 flex-none">
+    <img src="{pc.meta.imageURL}" alt="">
+  </div>
+  {/each}
+</div>
+
+<script>
+  import State from '../js/State'
+
+  let podcasts = []
+  let loading = false
+
+  function getPodcasts() {
+    loading = true
+    State.dexieDB.podcasts.toArray().then(result => {
+      podcasts = result
+      loading = false
+    })
+  }
+
+  getPodcasts()
+</script>
