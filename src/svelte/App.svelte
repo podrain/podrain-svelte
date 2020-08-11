0db122d5ee0b55b848ec1f69423a047b271bb43e
Joe Sweeney <joe.sweeney224@gmail.com>
2020-08-11 08:55:08 -0600

    show podcast view

--- src/svelte/App.svelte
+++ src/svelte/App.svelte
@@ -5,11 +5,13 @@
   import Router, { wrap, replace } from 'svelte-spa-router'
   import PodcastList from './PodcastList.svelte'
   import Settings from './Settings.svelte'
+  import PodcastShow from './PodcastShow.svelte'
 
   const routes = {
     '/': wrap(PodcastList, () => {
       replace('/podcasts')
     }),
+    '/podcasts/:id': PodcastShow,
     '/podcasts': PodcastList,
     '/settings': Settings,
   }
--- src/svelte/PodcastList.svelte
+++ src/svelte/PodcastList.svelte
@@ -1,8 +1,14 @@
 <Layout>
   <div class="flex flex-wrap">
     {#each podcasts as pc}
-    <div class="w-1/3 flex-none">
-      <img src="{pc.meta.imageURL}" alt="">
+    <div 
+      on:click={() => push('/podcasts/'+pc._id)}
+      class="w-1/3 flex-none"
+    >
+      <img 
+        src="{pc.meta.imageURL}"
+        alt="{pc.meta.title}"
+      >
     </div>
     {/each}
   </div>
@@ -11,6 +17,7 @@
 <script>
   import State from '../js/State'
   import Layout from './Layout.svelte'
+  import { push } from 'svelte-spa-router'
 
   let podcasts = []
   let loading = false
